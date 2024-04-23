63b703373fcb56bb297fe9524101871d4056361b
Christianguarnerosx <christianguarnerosx@gmail.com>
2024-04-22 20:52:31 -0600

    Agregar fondos de interfaz

--- Cliente/componentes_php/componente_elegir_fondo.php
+++ Cliente/componentes_php/componente_elegir_fondo.php
@@ -3,8 +3,10 @@
 
     <div class="row centrar">
         <img class="fondo-elegir espacio-top-c" src="../imagenes/fondos/fondo-gusano.gif" alt="" onclick="Fondoseleccionado('../imagenes/fondos/fondo-gusano.gif')">
+        <img class="fondo-elegir espacio-top-c" src="../imagenes/fondos/fondo-bmobano.gif" alt="" onclick="Fondoseleccionado('../imagenes/fondos/fondo-bmobano.gif')">
+        <img class="fondo-elegir espacio-top-c" src="../imagenes/fondos/fondorosadulceprincesa.gif" alt="" onclick="Fondoseleccionado('../imagenes/fondos/fondorosadulceprincesa.gif')">
+        <img class="fondo-elegir espacio-top-c" src="../imagenes/fondos/fondotronquitos.gif" alt="" onclick="Fondoseleccionado('../imagenes/fondos/fondotronquitos.gif')">
         <img class="fondo-elegir espacio-top-c" src="../imagenes/fondos/fondo-zen.gif" alt="" onclick="Fondoseleccionado('../imagenes/fondos/fondo-zen.gif')">
         <img class="fondo-elegir espacio-top-c" src="../imagenes/fondos/fondo-bmobaraba.gif" alt="" onclick="Fondoseleccionado('../imagenes/fondos/fondo-bmobaraba.gif')">
-        <img class="fondo-elegir espacio-top-c" src="../imagenes/fondos/fondo-bmobano.gif" alt="" onclick="Fondoseleccionado('../imagenes/fondos/fondo-bmobano.gif')">
         <h1 class="fondo-elegir espacio-top-c" onclick="Fondoseleccionado('')"> Sin fondo </h1>
     </div>
