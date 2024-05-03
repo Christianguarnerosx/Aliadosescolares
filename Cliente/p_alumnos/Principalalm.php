c36c94469898187125551c3e4474901844ae1be8
Christianguarnerosx <christianguarnerosx@gmail.com>
2024-05-02 18:52:57 -0600

    Actualizar comportamiento contenedor de cards y opciones

--- Cliente/p_alumnos/Principalalm.php
+++ Cliente/p_alumnos/Principalalm.php
@@ -56,7 +56,7 @@
 
                     <!-- Este es el contendor de las card/opciones que muestras acciones de la app en la interfaz principal-->
                     <!-- A este main se le aplicara un scroll en y cuando el contenido se deborde, solo se oculta el scroll x y se asigna un tama;o-->
-                    <main id="main-principalalm" class="row scroll-y"> <!--Para que pueda funcionar el centrar y evitar otro juego de col y row, se convierte el main en row y centramos su contenido-->
+                    <main id="main-principalalm" class="scroll-y"> <!--Para que pueda funcionar el centrar y evitar otro juego de col y row, se convierte el main en row y centramos su contenido-->
                         <div class="col"> <!-- AQUI VA EL CONTENIDO PROPIO -->
                             <div class="row espacio-top-c">
                                 <!--Fila/seccion de titulos y subtitulos-->
@@ -82,6 +82,38 @@
                                         <h1 class="text-m">Comunidad</h1>
                                         <p class="text-c">Reporte aliado</p>
                                         <img class="icono-card-rb" src="../imagenes/iconos/cards/comunidadblanco.png" alt="">
+                                    </div>
+                                </a>
+                                <!--Tarjeta de opciones de inicio/ en inetrfaz principal-->
+                                <a href="Desarrolloestadisticas.php" class="card-principalalm decoracion-no borde-r-c"> <!-- Esta Es la tarjeta tipo a, para que sea un link cuando le den click, el espacion parq ue existan entre ellas no funciona si le pones la class espacio top, se agrego al class card perosnalizado-->
+                                    <div class="row espacio-top-m"><!--Fila de texto de la card-->
+                                        <h1 class="text-m">Rendimiento</h1>
+                                        <p class="text-c">Estadisticas</p>
+                                        <img class="icono-card-rb" src="../imagenes/iconos/cards/graficosblanco.png" alt="">
+                                    </div>
+                                </a>
+                                <!--Tarjeta de opciones de inicio/ en inetrfaz principal-->
+                                <a href="Reportesestadistica.php" class="card-principalalm decoracion-no borde-r-c"> <!-- Esta Es la tarjeta tipo a, para que sea un link cuando le den click, el espacion parq ue existan entre ellas no funciona si le pones la class espacio top, se agrego al class card perosnalizado-->
+                                    <div class="row espacio-top-m"><!--Fila de texto de la card-->
+                                        <h1 class="text-m"> Aportes </h1>
+                                        <p class="text-c"> Tus reportes generados </p>
+                                        <img class="icono-card-rb" src="../imagenes/iconos/cards/reporteblanco.png" alt="">
+                                    </div>
+                                </a>
+                                <!--Tarjeta de opciones de inicio/ en inetrfaz principal-->
+                                <a href="Desarrolloestadisticas.php" class="card-principalalm decoracion-no borde-r-c"> <!-- Esta Es la tarjeta tipo a, para que sea un link cuando le den click, el espacion parq ue existan entre ellas no funciona si le pones la class espacio top, se agrego al class card perosnalizado-->
+                                    <div class="row espacio-top-m"><!--Fila de texto de la card-->
+                                        <h1 class="text-m">Rendimiento</h1>
+                                        <p class="text-c">Estadisticas</p>
+                                        <img class="icono-card-rb" src="../imagenes/iconos/cards/graficosblanco.png" alt="">
+                                    </div>
+                                </a>
+                                <!--Tarjeta de opciones de inicio/ en inetrfaz principal-->
+                                <a href="Reportesestadistica.php" class="card-principalalm decoracion-no borde-r-c"> <!-- Esta Es la tarjeta tipo a, para que sea un link cuando le den click, el espacion parq ue existan entre ellas no funciona si le pones la class espacio top, se agrego al class card perosnalizado-->
+                                    <div class="row espacio-top-m"><!--Fila de texto de la card-->
+                                        <h1 class="text-m"> Aportes </h1>
+                                        <p class="text-c"> Tus reportes generados </p>
+                                        <img class="icono-card-rb" src="../imagenes/iconos/cards/reporteblanco.png" alt="">
                                     </div>
                                 </a>
                                 <!--Tarjeta de opciones de inicio/ en inetrfaz principal-->
