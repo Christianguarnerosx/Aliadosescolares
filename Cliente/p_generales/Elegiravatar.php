fccdb68ba07925713a4fe760a6f0038848e0ae2a
Christianguarnerosx <christianguarnerosx@gmail.com>
2025-03-22 19:01:09 -0600

    Actualizar iconos de todas las interfaces

--- Cliente/p_generales/Elegiravatar.php
+++ Cliente/p_generales/Elegiravatar.php
@@ -6,6 +6,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- icono de la pagina -->
+     <link rel="icon" type="image/x-icon" href="../imagenes/logos/logoaliados.png">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
